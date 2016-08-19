bc1ac7be9f115aa335c2157d4d473c0f378a0d03
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-08-19 10:58:37 +1000

    Commit 1
    first Commit for file cgCTL

--- src/datamanagement/cgCTL.java
+++ src/datamanagement/cgCTL.java
@@ -1,34 +1,40 @@
+/** Displaying the package in which the class resides */
 package datamanagement;
+/**
+* This class allows the instantiation of cgCTL objects
+* which can invoke the following methods:
+* execute, unitSelected, studentSelected, checkGrade, enableChangeMarks, and saveGrade.
+*/
 
+/** Start of the class cgCTL */
 public class cgCTL {
+	cgUI CGUI; // Declare the object named CGUI of type cgUI 
+	String cuc = null; // Declare and initialize the variable cuc to null 
+	Integer currentStudentID = null; // Declare and initialize the variable currentStudentID to null 
+	boolean changed = false; // Declare and initialize the variable changed to false 
 
-	cgUI CGUI;
-	String cuc = null;
-	Integer currentStudentID = null;
-	boolean changed = false;
-
-	public cgCTL() {
+	/** The no-args constructor for an object cgCTL */
+	public cgCTL(){
 	}
 
+	/** Start of the method execute..... */
 	public void execute() {
 		CGUI = new cgUI(this);
 		CGUI.setState1(false);
-
 		CGUI.setState2(false);
 		CGUI.setState3(false);
 		CGUI.setState4(false);
 		CGUI.setState5(false);
 		CGUI.setState6(false);
 		CGUI.Refresh3();
-
 		ListUnitsCTL luCTL = new ListUnitsCTL();
 		luCTL.listUnits(CGUI);
 		CGUI.setVisible(true);
 		CGUI.setState1(true);
-	}
+	} // End of the method execute 
 
+	/** The method unitSelected.....*/
 	public void unitSelected(String code) {
-
 		if (code.equals("NONE"))
 			CGUI.setState2(false);
 		else {
@@ -38,8 +44,9 @@ public class cgCTL {
 			CGUI.setState2(true);
 		}
 		CGUI.setState3(false);
-	}
+	} // End of the method unitSelected
 
+	/** Start of the method studentSelected....*/
 	public void studentSelected(Integer id) {
 		currentStudentID = id;
 		if (currentStudentID.intValue() == 0) {
@@ -49,21 +56,17 @@ public class cgCTL {
 			CGUI.setState5(false);
 			CGUI.setState6(false);
 		}
-
 		else {
 			IStudent s = StudentManager.get().getStudent(id);
-
 			IStudentUnitRecord r = s.getUnitRecord(cuc);
-
 			CGUI.setRecord(r);
 			CGUI.setState3(true);
 			CGUI.setState4(true);
 			CGUI.setState5(false);
 			CGUI.setState6(false);
 			changed = false;
-
 		}
-	}
+	} // End of the method studentSelected
 
 	public String checkGrade(float f, float g, float h) {
 		IUnit u = UnitManager.UM().getUnit(cuc);
@@ -76,18 +79,18 @@ public class cgCTL {
 		return s;
 	}
 
+	/** Start of the method enableChangeMarks....*/
 	public void enableChangeMarks() {
 		CGUI.setState4(false);
 		CGUI.setState6(false);
 		CGUI.setState5(true);
 		changed = true;
-	}
+	} // End of the method enableChangeMarks
 
+	/** Start of the method saveGrade....*/
 	public void saveGrade(float asg1, float asg2, float exam) {
-
 		IUnit u = UnitManager.UM().getUnit(cuc);
 		IStudent s = StudentManager.get().getStudent(currentStudentID);
-
 		IStudentUnitRecord r = s.getUnitRecord(cuc);
 		r.setAsg1(asg1);
 		r.setAsg2(asg2);
@@ -96,5 +99,5 @@ public class cgCTL {
 		CGUI.setState4(true);
 		CGUI.setState5(false);
 		CGUI.setState6(false);
-	}
-}
+	} // End of the method saveGrade
+} // End of the class cgCTL 
