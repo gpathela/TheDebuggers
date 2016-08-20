e34aafb4e2a17f212e68e39c1365e8c13c2330de
rpreet <rpreet72@gmail.com>
2016-08-20 17:47:47 +1000

    class ListStudentsCtl
    change class name from ListStudentsCTL to ListStudentsCtl as
    abbreviations and acronyms should not be uppercase

--- src/datamanagement/ListUnitsCTL.java
+++ src/datamanagement/ListUnitsCTL.java
@@ -1,10 +1,24 @@
+/** 
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @date 20.08.2016
+*/
 package datamanagement;
-public class ListUnitsCTL {
+
+/** change class name from ListStudentsCTL to ListStudentsCtl 
+* as abbreviations and acronyms should not be uppercase
+*/
+public class ListStudentsCtl {		//Start of the class
     private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.UM();
-}
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
+	/** Construst a default ListStudentsCtl object*/
+	public ListStudentsCtl() {		
+      um = UnitManager.UM();
+	}
+    public void listUnits(IUnitLister lister){	//Start of the method
+	  lister.clearUnits();UnitMap units = um.getUnits();
+      for (String s : units.keySet())
+          lister.addUnit(units.get(s));
+	  }//End of the method
+}//End of the class
