7451de3f5e7a1e7888d3d48f529b554110cca364
rpreet <rpreet72@gmail.com>
2016-08-20 17:45:23 +1000

    interface IUnitLister
    add some comments in this file

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -1,8 +1,25 @@
+/** 
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @date 20.08.2016
+*/
 package datamanagement;
-        public class ListStudentsCTL {
-private StudentManager sm;
-public ListStudentsCTL() {sm = StudentManager.get();}
-            public void listStudents( IStudentLister lister, String unitCode ) {
-    lister.clearStudents();
-                StudentMap students = sm.getStudentsByUnit( unitCode );
-for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
+
+/** change class name from ListStudentsCTL to ListStudentsCtl 
+* as abbreviations and acronyms should not be uppercase
+*/
+public class ListStudentsCtl {		//Start of the class
+  private StudentManager sm;		
+  /** Construst a default ListStudentsCtl object*/
+  public ListStudentsCtl() {
+	  sm = StudentManager.get();
+	  }
+  public void listStudents(IStudentLister lister, String unitCode) {
+      lister.clearStudents();
+      StudentMap students = sm.getStudentsByUnit( unitCode );
+      for (Integer id : students.keySet()) 
+	  lister.addStudent(students.get(id));
+      }
+}//End of the class
