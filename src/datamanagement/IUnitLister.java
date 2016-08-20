a7f0b6a502be5b0709adb06b7dc58d4d401a356c
rpreet <rpreet72@gmail.com>
2016-08-20 15:04:34 +1000

    Commits in IUnitLister
    Add some comments in this file

--- src/datamanagement/IUnitLister.java
+++ src/datamanagement/IUnitLister.java
@@ -1,12 +1,17 @@
 /**
-
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @date 20.08.2016
 */
 
 package datamanagement;
 
+//Start of interface
 public interface IUnitLister {
 
-    public void clearUnits();
-    public void addUnit(IUnit unit);
+    public void clearUnits();		//implicitly abstract method
+    public void addUnit(IUnit unit);	//implicitly abstract method
 
-}
+}//End of interface
