eafa5ae6322f9db9909d377cbd6386d68a549f50
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-08-21 09:13:28 +1000

    The AppProperties class
    Contains header and end of line comments explaining the variables,
    contructor, and the methods created in the class

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -1,14 +1,49 @@
+/** Import some Java libraries and specify the package in which the class resides */
 package datamanagement;
 import java.util.*;
 import java.io.*;
-public class AppProperties {
-private static AppProperties self = null;
-private Properties properties;
+/**
+*@reviewer Jean Claude Jino Rousseau
+*@course Master of Information Technology
+*@subject Professional Programming Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 26.08.2016
+*@version 1.2
+*
+*The AppProperties class is a program that uses private variables to create instances
+*with a no-arg constructor. It has two methods, namely the getInstance, and getProperties
+*methods which can be used to return an instance of the class or its properties.
+*/
 
-public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
-private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
-catch (IOException e) {throw new RuntimeException("Could not read property file");}}
-    public Properties getProperties() {return properties;
-}}
+/** Start of the class AppProperties */
+public class AppProperties{
+	/** Declaring and initialize the variable for the class
+		with private visibility */
+	private static AppProperties self = null; // Declare and initialize the variable self to null 
+	private Properties properties;
+
+	/** The no-args constructor which contains  a try and catch
+		method, is used to create an object of an AppProperties */
+	private AppProperties(){
+		properties = new Properties();
+		try{
+			properties.load(new FileInputStream("Properties.prop"));
+			} 
+		catch(IOException e){
+			throw new RuntimeException("Could not read property file");
+			}
+	} // End of the no-arg constructor
+	
+	/** An instance of the AppProperties class can use the getInstance method to return itself */
+	public static AppProperties getInstance(){
+		if(self == null ){
+			self = new AppProperties();
+		} 
+		return self;
+	} // End of the getInstance method 
+
+	/** The getProperties method returns the properties of an object of AppProperties*/
+	public Properties getProperties(){
+		return properties;
+	} // End of the getProperties method 
+} // End of the class AppProperties
--- src/datamanagement/cgCTL.java
+++ src/datamanagement/cgCTL.java
@@ -1,89 +1,91 @@
 /** Displaying the package in which the class resides */
 package datamanagement;
 /**
-* This class allows the instantiation of cgCTL objects
+* This class allows the instantiation of CgCtl objects
 * which can invoke the following methods:
 * execute, unitSelected, studentSelected, checkGrade, enableChangeMarks, and saveGrade.
 */
 
-/** Start of the class cgCTL */
-public class cgCTL {
-	cgUI CGUI; // Declare the object named CGUI of type cgUI 
+/** Start of the class CgCtl. The class name should start with a capital letter. 
+	Following the Java convention, the desired class name would be as follows CgCtl */
+public class CgCtl {
+	CgUI cgUI; // Declare the object named CGUI of type cgUI 
 	String cuc = null; // Declare and initialize the variable cuc to null 
 	Integer currentStudentID = null; // Declare and initialize the variable currentStudentID to null 
 	boolean changed = false; // Declare and initialize the variable changed to false 
 
-	/** The no-args constructor for an object cgCTL */
-	public cgCTL(){
+	/** The no-args constructor for an object CgCtl */
+	public CgCtl(){
 	}
 
 	/** Start of the method execute..... */
 	public void execute() {
-		CGUI = new cgUI(this);
-		CGUI.setState1(false);
-		CGUI.setState2(false);
-		CGUI.setState3(false);
-		CGUI.setState4(false);
-		CGUI.setState5(false);
-		CGUI.setState6(false);
-		CGUI.Refresh3();
+		cgUI = new CgUI(this);
+		cgUI.setState1(false);
+		cgUI.setState2(false);
+		cgUI.setState3(false);
+		cgUI.setState4(false);
+		cgUI.setState5(false);
+		cgUI.setState6(false);
+		cgUI.Refresh3();
 		ListUnitsCTL luCTL = new ListUnitsCTL();
-		luCTL.listUnits(CGUI);
-		CGUI.setVisible(true);
-		CGUI.setState1(true);
+		luCTL.listUnits(cgUI);
+		cgUI.setVisible(true);
+		cgUI.setState1(true);
 	} // End of the method execute 
 
 	/** The method unitSelected.....*/
 	public void unitSelected(String code) {
 		if (code.equals("NONE"))
-			CGUI.setState2(false);
+			cgUI.setState2(false);
 		else {
 			ListStudentsCTL lsCTL = new ListStudentsCTL();
-			lsCTL.listStudents(CGUI, code);
+			lsCTL.listStudents(cgUI, code);
 			cuc = code;
-			CGUI.setState2(true);
+			cgUI.setState2(true);
 		}
-		CGUI.setState3(false);
+		cgUI.setState3(false);
 	} // End of the method unitSelected
 
 	/** Start of the method studentSelected....*/
 	public void studentSelected(Integer id) {
 		currentStudentID = id;
 		if (currentStudentID.intValue() == 0) {
-			CGUI.Refresh3();
-			CGUI.setState3(false);
-			CGUI.setState4(false);
-			CGUI.setState5(false);
-			CGUI.setState6(false);
+			cgUI.Refresh3();
+			cgUI.setState3(false);
+			cgUI.setState4(false);
+			cgUI.setState5(false);
+			cgUI.setState6(false);
 		}
 		else {
 			IStudent s = StudentManager.get().getStudent(id);
 			IStudentUnitRecord r = s.getUnitRecord(cuc);
-			CGUI.setRecord(r);
-			CGUI.setState3(true);
-			CGUI.setState4(true);
-			CGUI.setState5(false);
-			CGUI.setState6(false);
+			cgUI.setRecord(r);
+			cgUI.setState3(true);
+			cgUI.setState4(true);
+			cgUI.setState5(false);
+			cgUI.setState6(false);
 			changed = false;
 		}
 	} // End of the method studentSelected
 
+	/** Start of the checkGrade method */
 	public String checkGrade(float f, float g, float h) {
 		IUnit u = UnitManager.UM().getUnit(cuc);
 		String s = u.getGrade(f, g, h);
-		CGUI.setState4(true);
-		CGUI.setState5(false);
+		cgUI.setState4(true);
+		cgUI.setState5(false);
 		if (changed) {
-			CGUI.setState6(true);
+			cgUI.setState6(true);
 		}
 		return s;
-	}
+	} // End of the checkGrade method 
 
 	/** Start of the method enableChangeMarks....*/
 	public void enableChangeMarks() {
-		CGUI.setState4(false);
-		CGUI.setState6(false);
-		CGUI.setState5(true);
+		cgUI.setState4(false);
+		cgUI.setState6(false);
+		cgUI.setState5(true);
 		changed = true;
 	} // End of the method enableChangeMarks
 
@@ -96,8 +98,8 @@ public class cgCTL {
 		r.setAsg2(asg2);
 		r.setExam(exam);
 		StudentUnitRecordManager.instance().saveRecord(r);
-		CGUI.setState4(true);
-		CGUI.setState5(false);
-		CGUI.setState6(false);
+		cgUI.setState4(true);
+		cgUI.setState5(false);
+		cgUI.setState6(false);
 	} // End of the method saveGrade
 } // End of the class cgCTL 
