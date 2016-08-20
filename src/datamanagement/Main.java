736e5864323dbd80b61693a1864561826844effa
rpreet <rpreet72@gmail.com>
2016-08-20 17:49:26 +1000

    class Main
    add some comments in this file

--- src/datamanagement/Main.java
+++ src/datamanagement/Main.java
@@ -1,4 +1,16 @@
+/** 
+* @author Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @date 20.08.2016
+*/
 package datamanagement;
 
+/** Start of the class*/
 public class Main {
-    public static void main(String[] p) {new cgCTL().execute();}    }
+	/** Start of the main method */
+    public static void main(String[] p){
+	  new cgCTL().execute();
+	}// End of the main method   
+}//End of the class
