bd519ee8c2ca14a98c5db4649278d5f212727b55
gpathela <gpathela@yahoo.com>
2016-08-24 21:25:46 +1000

    Version 1.3 Change to meet java convention
    Change of variables and methods according to Java Conventions.

--- src/datamanagement/StudentUnitRecordProxy.java
+++ src/datamanagement/StudentUnitRecordProxy.java
@@ -7,53 +7,60 @@ package datamanagement;
  * @subject Professional Programming Practice
  * @lecturer Dr Recep Ulusoy
  * @due date 26.08.2016
- * @version 1.1
+ * @version 1.3
  * 
+ *          StudentUnitRecordProxy class implements IStudentUnitRecord . The
+ *          main purpose of class is to get and set marks of assesments and
+ *          exams.
+ *
  */
+
+// Start of StudentUnitRecordProxy Class
 public class StudentUnitRecordProxy implements IStudentUnitRecord {
-	private Integer studentID;
-	private String unitCode;
-	private StudentUnitRecordManager mngr;
+	private Integer studentId_; // Class Private Variables
+	private String unitCode_;
+	private StudentUnitRecordManager studentUnitRecordManager_;
 
-	public StudentUnitRecordProxy(Integer id, String code) {
-		this.studentID = id;
-		this.unitCode = code;
-		this.mngr = StudentUnitRecordManager.instance();
+	public StudentUnitRecordProxy(Integer studentId, String unitCode) { // Public
+																		// Constructor
+		this.studentId_ = studentId;
+		this.unitCode_ = unitCode;
+		this.studentUnitRecordManager_ = StudentUnitRecordManager.instance();
 	}
 
-	public Integer getStudentID() {
-		return studentID;
+	public Integer getStudentId() { // Method to get student Id
+		return studentId_;
+	} // End of method to get Student Id
+
+	public String getUnitCode() { // Method to get Unit Code
+		return unitCode_;
+	} // End of method to get unitCode
+
+	public void setAsg1(float mark) { // Method to set marks of assignment 1
+		studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).setAsg1(mark);
 	}
 
-	public String getUnitCode() {
-		return unitCode;
+	public float getAsg1() { // Method to get marks of assignment 1
+		return studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).getAsg1();
 	}
 
-	public void setAsg1(float mark) {
-		mngr.getStudentUnitRecord(studentID, unitCode).setAsg1(mark);
+	public void setAsg2(float mark) { // Method to set marks of assignment 2
+		studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).setAsg2(mark);
 	}
 
-	public float getAsg1() {
-		return mngr.getStudentUnitRecord(studentID, unitCode).getAsg1();
+	public float getAsg2() { // Method to get marks of assignment 2
+		return studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).getAsg2();
 	}
 
-	public void setAsg2(float mark) {
-		mngr.getStudentUnitRecord(studentID, unitCode).setAsg2(mark);
+	public void setExam(float mark) { // Method to set marks of exam
+		studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).setExam(mark);
 	}
 
-	public float getAsg2() {
-		return mngr.getStudentUnitRecord(studentID, unitCode).getAsg2();
+	public float getExam() { // Method to get marks of exam
+		return studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).getExam();
 	}
 
-	public void setExam(float mark) {
-		mngr.getStudentUnitRecord(studentID, unitCode).setExam(mark);
-	}
-
-	public float getExam() {
-		return mngr.getStudentUnitRecord(studentID, unitCode).getExam();
-	}
-
-	public float getTotal() {
-		return mngr.getStudentUnitRecord(studentID, unitCode).getTotal();
+	public float getTotal() { // Method to get total marks
+		return studentUnitRecordManager_.getStudentUnitRecord(studentID, unitCode).getTotal();
 	}
 }
